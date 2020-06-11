635ea561f67d3bc85b58c985ce6eb14293eb173c
vbalan <vbalan@patriot.ambianta.local>
2020-06-11 08:39:34 +0300

    final

--- src/ide/DeveloperAssistantPlugin.java
+++ src/ide/DeveloperAssistantPlugin.java
@@ -1,9 +1,8 @@
 package ide;
 
-import java.lang.reflect.Method;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.stream.Stream;
+import java.util.Arrays;
 
 import source.Button;
 
@@ -12,16 +11,20 @@ public class DeveloperAssistantPlugin {
 	public static List<String> getSuggestions(Class<?> sourceClass) {
 		
 		if (sourceClass == Button.class) {
+			
 			List<String> methodList = new ArrayList<String>();
-			Method[] methods = sourceClass.getDeclaredMethods();
-			Stream.of(methods).forEach(method -> methodList.add(method.getName()));
+
+			Arrays.asList(sourceClass.getDeclaredMethods()).forEach(method -> methodList.add(method.getName()));
+						
 			return methodList;
 		}
 		
 		List<String> List = new ArrayList<String>();
-		Method[] methods = sourceClass.getDeclaredMethods();
-		Stream.of(methods).forEach(method -> List.add(method.getName()));
+		
+		Arrays.asList(sourceClass.getDeclaredMethods()).forEach(method -> List.add(method.getName()));
+		
 		List.addAll(getSuggestions(sourceClass.getSuperclass()));
+		
 		return List;
 	}
 }
